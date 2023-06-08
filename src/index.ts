3e026d45d50b2c34ec4b7ba3173b440e066f949c
the-redback <maruf.2hin@gmail.com>
2023-06-08 10:45:07 -0500

    updated code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,46 @@
-import {createServer, IncomingMessage, ServerResponse} from 'http';
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
 
-const server = createServer(
-  (request: IncomingMessage, response: ServerResponse) => {
-    response.end('Hello world from updated code!');
-  }
-);
+const PORT = parseInt(String(process.env.PORT), 10) || 3000;
+const app = express();
 
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000/');
+app.use(bodyParser.json());
+
+app.post('/login', (req: express.Request, res: express.Response) => {
+  const {username, password} = req.body;
+  console.log('Login user: ', username, password);
+  const msg = {
+    action: 'LOGIN',
+    data: {username, password},
+    time: Date().toString(),
+  };
+
+  console.log('/login',msg)
+  return res.send(msg);
+});
+
+app.post('/tasks', (req: express.Request, res: express.Response) => {
+  const {msg} = req.body;
+
+  console.log('/tasks', msg);
+  return res.send(msg);
+});
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  console.log('/')
+  res.send('Hello World');
+});
+
+app.get('/send-msg', (req: express.Request, res: express.Response) => {
+  const data = {
+    msg: 'Hello from the other side!!',
+    time: Date().toString(),
+  };
+
+  console.log('/send-msg', data);
+  res.send(data);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}/`);
 });
